78afda21d1f705b51014de471fc3f2fc312735ff
truongle <htr.lee@gmail.com>
2018-04-05 00:10:34 +0700

    change precompile manifest

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -8,7 +8,7 @@ Rails.application.config.assets.version = '1.0'
 # Add Yarn node_modules folder to the asset load path.
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
 Rails.application.config.assets.compile = true
-Rails.application.config.assets.precompile =  ['*.js', '*.css', '*.css.scss'] 
+Rails.application.config.assets.precompile =  ['application.js', 'application.css.scss']
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
