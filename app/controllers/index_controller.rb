f02c1a97f026246891d3de6edcc330eaf3d4ace9
truongle <htr.lee@gmail.com>
2018-04-09 09:53:25 +0700

    fix main page

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -2,14 +2,14 @@ class IndexController < ApplicationController
   before_action :set_product, only: [:product_details]
 
   def index
-    @q = Product.ransack(params[:q])
+    @q = Product.is_not_draft.ransack(params[:q])
     @products = @q.result().includes(:product_type).page(params[:page]).per(15)
   end
-
+  
   def product_details
 
   end
-  
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_product
