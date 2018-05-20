76b28c6c6ba20b22e4f245aefb5cf5214c85694a
truongle <htr.lee@gmail.com>
2018-05-20 11:37:49 +0700

    set up environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,4 +88,14 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  
+  config.paperclip_defaults = {
+    storage: :s3,
+    s3_credentials: {
+      bucket: ENV['S3_BUCKET_NAME_DEV'],
+      access_key_id: ENV['AWS_ACCESS_KEY_ID_DEV'],
+      secret_access_key: ENV['AWS_SECRET_ACCESS_KEY_DEV'],
+      s3_region: ENV['AWS_REGION_DEV'],
+    }
+  }
 end
