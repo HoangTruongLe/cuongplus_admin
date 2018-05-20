31d5c756cbbbcda5ec3458d5557442233ebf16fc
truongle <htr.lee@gmail.com>
2018-05-20 20:18:23 +0700

    change seed

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,7 +7,9 @@ set :deploy_to, '/home/ubuntu/cuongplus_admin'
 append :linked_files, "config/database.yml", "config/secrets.yml"
 append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "vendor/bundle", "public/system", "public/uploads"
 load 'lib/seed.rb'
-after 'deploy:migrating', :seed
+# after 'deploy:migrating', :seed
+
+
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
